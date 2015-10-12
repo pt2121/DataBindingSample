6ba854cd327a9811d0c5c08ea99f3417f6ec9cca
Prat <banqfirn@gmail.com>
2015-10-12 08:23:18 -0400

    Clean up

--- mvvm/src/main/java/com/prt2121/mvvm/MainActivity.java
+++ mvvm/src/main/java/com/prt2121/mvvm/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
